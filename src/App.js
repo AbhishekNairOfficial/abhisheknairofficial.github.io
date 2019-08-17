9d6a96c47f74e27c89b5a1b8ca037c65db3185af
Abhishek P <abhishek.p@piktorlabs.com>
2019-08-17 15:33:53 +0530

    Removed Email Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import LandingPage from './pages/landing-page';
 import ProjectsPage from './pages/projects-page';
-import EmailPage from './pages/email-page';
 import './App.css';
 
 const App = () => {
@@ -33,7 +32,6 @@ const App = () => {
     <div className="container">
       <LandingPage />
       <ProjectsPage />
-      <EmailPage />
     </div>
   );
 };
