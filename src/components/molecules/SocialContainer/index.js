0df956bc62e1c8c040bc7192c0c3f1ca81a76e34
Abhishek P <abhishek.p@piktorlabs.com>
2020-06-06 23:25:09 +0530

    Moved social links to firebase

--- src/components/molecules/SocialContainer/index.js
+++ src/components/molecules/SocialContainer/index.js
@@ -1,6 +1,9 @@
 /* eslint-disable comma-dangle */
 import React from 'react';
 import styled from 'styled-components';
+import Skeleton from 'react-loading-skeleton';
+
+import { useRealtimeDatabase } from '../../../config/useFirebase';
 
 import QUORA_ICON from '../../../assets/images/quora.svg';
 import LINKEDIN_ICON from '../../../assets/images/linkedin.svg';
@@ -14,26 +17,34 @@ export const Image = styled.img`
   margin-left: 30px;
 `;
 
-const socialProfileMaps = [
-  { icon: QUORA_ICON, link: 'https://www.quora.com/profile/Abhishek-Nair-147' },
-  { icon: LINKEDIN_ICON, link: 'https://www.linkedin.com/in/6foot4/' },
-  { icon: MEDIUM_ICON, link: 'https://medium.com/@abhisheknairp' },
-  { icon: DRIBBBLE_ICON, link: 'https://dribbble.com/AbhishekNairOfficial' },
-  { icon: GITHUB_ICON, link: 'https://github.com/abhisheknairofficial' },
-  { icon: TWITTER_ICON, link: 'https://twitter.com/NairGalactico21' },
-];
+const imageLinks = {
+  quora: QUORA_ICON,
+  linkedin: LINKEDIN_ICON,
+  medium: MEDIUM_ICON,
+  dribbble: DRIBBBLE_ICON,
+  github: GITHUB_ICON,
+  twitter: TWITTER_ICON,
+};
 
-const SocialContainer = () => (
-  <div>
-    {socialProfileMaps.map((socialProfile, index) => {
-      const { icon, link } = socialProfile;
-      return (
-        <a href={link} key={index} target="_blank" rel="noopener noreferrer">
-          <Image src={icon} alt={link} />
-        </a>
-      );
-    })}
-  </div>
-);
+const SocialContainer = () => {
+  const socialProfiles = useRealtimeDatabase('socialProfiles');
+
+  return (
+    <div>
+      {socialProfiles ? (
+        socialProfiles.map((socialProfile, index) => {
+          const { icon, link } = socialProfile;
+          return (
+            <a href={link} key={index} target="_blank" rel="noopener noreferrer">
+              <Image src={imageLinks[icon]} alt={link} />
+            </a>
+          );
+        })
+      ) : (
+        <Skeleton count={6} height={32} width={32} />
+      )}
+    </div>
+  );
+};
 
 export default SocialContainer;
