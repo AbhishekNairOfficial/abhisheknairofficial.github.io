60ced4c90156b764216e279bf35cdbc44abeb055
Abhishek P <abhishek.p@piktorlabs.com>
2020-09-17 19:04:50 +0530

    Fixed footer

--- src/components/templates/Footer/index.js
+++ src/components/templates/Footer/index.js
@@ -4,11 +4,11 @@ import styled from 'styled-components';
 import SocialContainer from 'components/molecules/SocialContainer';
 
 export const Container = styled.footer`
-  background-color: ${props => props.theme.palette.white[3]};
+  background-color: ${props => props.theme.palette.primary[0]};
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 2.5em 5vw;
+  padding: 2.5em 15vw;
   @media only screen and (max-width: 600px) {
     flex-direction: column;
     padding: 1.5em 20px;
@@ -18,11 +18,13 @@ export const Container = styled.footer`
 
 export const LeftSideText = styled.p`
   font-family: ${props => props.theme.fonts.primary};
+  color: ${props => props.theme.palette.white[1]};
+  font-size: 1.1em;
 `;
 
 const Footer = () => (
   <Container>
-    <LeftSideText>© 2020 by Abhishek P. All rights reserved.</LeftSideText>
+    <LeftSideText>© 2020 by Abhishek P</LeftSideText>
     <SocialContainer />
   </Container>
 );
