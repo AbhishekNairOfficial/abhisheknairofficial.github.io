14198ec1a00e2a94622f654a693b520286564428
Abhishek P <abhishek.p@piktorlabs.com>
2021-04-20 22:34:28 +0530

    Updated image sizes

--- src/atoms/BannerImage/index.js
+++ src/atoms/BannerImage/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const Image = styled.img`
+export const Picture = styled.picture`
   align-self: flex-end;
   background-image: transparent url;
+  @media only screen and (max-width: 600px) {
+    margin-top: 10vh;
+  }
+`;
+
+export const Image = styled.img`
   border-radius: 250px;
   height: 300px;
   @media only screen and (max-width: 600px) {
     width: 80%;
     height: auto;
-    margin-top: 10vh;
   }
 `;
 
@@ -22,9 +27,15 @@ export const StyledSkeleton = styled.div`
 
 const BannerImage = () => {
   // Hooks
-  const imageUrl = '/banner/banner-image.jpg';
+  const imageUrl = '/banner/banner-image.jpg?resize&size=300';
 
-  return <Image src={imageUrl} alt="Banner" />;
+  return (
+    <Picture>
+      <source srcSet={`${imageUrl}&webp`} type="image/webp" />
+      <source srcSet={imageUrl} type="image/jpeg" />
+      <Image src={imageUrl} alt="Banner" />
+    </Picture>
+  );
 };
 
 export default BannerImage;
--- src/atoms/ImageComponent/index.js
+++ src/atoms/ImageComponent/index.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Picture = styled.picture`
+  margin-bottom: 15px;
+  &:last-child {
+    margin-bottom: 0px;
+  }
+`;
+
 const ImageContainer = styled.img`
   width: 100%;
   height: auto;
   object-fit: cover;
   width: calc((100vw - 6em - 30px) / 3);
-  margin-bottom: 15px;
-  &:last-child {
-    margin-bottom: 0px;
-  }
   @media only screen and (max-width: 600px) {
     width: calc(100vw - 60px);
   }
 `;
 
 const ImageComponent = ({ src }) => {
-  return <ImageContainer alt="about me" src={src} />;
+  return (
+    <Picture>
+      <source srcSet={`${src}?webp`} type="image/webp" />
+      <source srcSet={src} type="image/jpeg" />
+      <ImageContainer alt="about me" src={src} />
+    </Picture>
+  );
 };
 
 export default ImageComponent;
