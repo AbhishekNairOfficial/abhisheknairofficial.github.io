e5fe16b2b57a977da4c6f7bcd094bdcf728a8259
Abhishek P <abhishek.p@piktorlabs.com>
2021-05-11 13:54:51 +0530

    Banner cleaned up

--- src/atoms/6foot4/index.js
+++ src/atoms/6foot4/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import GridContainer from '../GridContainer';
 import styles from './index.module.css';
 
 const SixFootFour = () => (
   <div className={styles.container}>
     <span className={[styles.text, styles.left, styles.bottom].join(' ')}>6</span>
-    <GridContainer />
     <span className={[styles.text, styles.right, styles.top].join(' ')}>4</span>
   </div>
 );
