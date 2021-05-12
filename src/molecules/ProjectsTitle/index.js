4d62d4737eebdfc8e3fe1cf4033d9af2709181bb
Abhishek P <abhishek.p@piktorlabs.com>
2021-05-12 21:19:34 +0530

    Color change

--- src/molecules/ProjectsTitle/index.js
+++ src/molecules/ProjectsTitle/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import styles from './index.module.css';
 
-const ProjectsTitle = () => (
-  <div className={styles.container}>
-    <h1 className={styles.title}>Here is some of my best work</h1>
-  </div>
-);
+const ProjectsTitle = () => {
+  useEffect(() => {
+    const projectTitleElement = document.getElementById('projectsTitle');
+    const { top } = projectTitleElement.getBoundingClientRect();
+    const heightOfTheWindow = window.innerHeight;
+    if (top > heightOfTheWindow) {
+      // This component is below
+      projectTitleElement.classList.add(styles.black);
+    }
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1 id="projectsTitle" className={styles.title}>
+        Here is some of my best work
+      </h1>
+    </div>
+  );
+};
 
 export default ProjectsTitle;
