82de07f1a318465628f921fcf7896b953916ba9f
Abhishek P <abhishek.p@piktorlabs.com>
2021-05-12 21:25:05 +0530

    Added about me link

--- src/atoms/HeroText/index.js
+++ src/atoms/HeroText/index.js
@@ -11,12 +11,24 @@ const HeroText = () => {
   const { landingPage } = useContext(FirebaseContext);
   const { paraOne, paraTwo } = landingPage;
 
+  const onButtonClick = () => {
+    document
+      .getElementById('AboutMeSection')
+      .scrollIntoView({ behavior: 'smooth', block: 'center' });
+  };
+
   return (
     <div className={styles.text}>
       <h1 className={styles.helloText}>{greetingText()}</h1>
       <p>{paraOne}</p>
       <p>{paraTwo}</p>
-      <div className={styles.button}>
+      <div
+        tabIndex="0"
+        aria-hidden="true"
+        role="button"
+        onClick={onButtonClick}
+        className={styles.button}
+      >
         <span>More about me</span>
         <Image className="down-arrow" src="/banner/down-arrow.svg" height={16} width={11} />
       </div>
