1edd167f74c2c6a8049ea04358e40f366da321d8
Abhishek Nair <abhisheknr4@gmail.com>
2025-02-18 10:46:57 -0800

    Banner complete

--- src/app/atoms/Banner/Banner.tsx
+++ src/app/atoms/Banner/Banner.tsx
@@ -1,6 +1,11 @@
+import { JOB_DESCRIPTION, JOB_TITLE } from "@/config/constants"
+
 const Banner = () => (
-    <section className="mx-[50px] p-[50px] bg-secondary">
-        Hi
+    <section className="mx-[50px] p-[50px] bg-secondary flex justify-center">
+        <h1 className="text-right font-primary text-7xl flex-1">
+            <p className="inline bg-white px-2 leading-[1.5]">{JOB_TITLE}</p>
+        </h1>
+        <p className="flex-1 pl-[50px] text-2xl">{JOB_DESCRIPTION}</p>
     </section>
 )
 
---
+++ src/config/constants.ts
@@ -0,0 +1,3 @@
+export const JOB_TITLE = "Senior Product Technologist";
+export const JOB_DESCRIPTION =
+  "Lead Frontend Engineer with 7+ years of experience specializing in React.js, Next.js, and enterprise-scale web applications.\nExpert in UX implementation, RESTful APIs, and modern frontend architectures.";
