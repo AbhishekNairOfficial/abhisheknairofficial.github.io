4878e52732dadb202b7bdf97553a2348fc85e793
AbhishekNairOfficial <abhisheknr4@gmail.com>
2025-10-27 18:49:04 +0530

    Removing cache

--- src/app/api/spotify/route.ts
+++ src/app/api/spotify/route.ts
@@ -14,7 +14,7 @@ export async function GET(): Promise<NextResponse> {
       {
         status: HTTP_STATUS_OK,
         headers: {
-          'Cache-Control': 'public, s-maxage=60, stale-while-revalidate=30',
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
         },
       });
   }
@@ -37,7 +37,7 @@ export async function GET(): Promise<NextResponse> {
     {
       status: HTTP_STATUS_OK,
       headers: {
-        'Cache-Control': 'public, s-maxage=60, stale-while-revalidate=30',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
       },
     },
   );
